32209eeb2f4b0eb85adf27e73a585ae8717429fd
Juan Manuel Alegre <alegre.juan1990@gmail.com>
2022-03-09 20:21:31 -0300

    descripcion

---
+++ gitPrueba.js
@@ -0,0 +1,36 @@
+//Comandos
+//git clone http...repo.git sirve para clonar un repositorio de github
+//git init // genera un repositorio local en la carpeta donde estan los archivos
+//configuracion de mi repositorio
+//git config usar.name "Juanmagithb" // indico el usuario
+//git config usar.name te dice que usario esta vinculado al repositorio
+//git config user.email "alegre.juan1990@gmail.com" // indico el mail
+//git config user.email te dice que mail esta vinculado al repositorio
+//para configuracion unica 
+//git config --global usar.name "Juanmagithb"
+//git config --global user.email "alegre.juan1990@gmail.com"
+
+// para vincular un respositorio de la nube 
+//git remote add origin https://github.com/Juanmagithb/prueba.git
+
+//para desvincular git remote remove origin
+
+//para cambiar (master) //git branch -M elnombrequelequieraponer
+//git branch -M main / git branch -M master / 
+
+//git status
+
+//git add . // informacion del repo
+
+//git commit -m "descripcion de los cambios/version/etc"
+
+//git log // informacion del repo
+
+//para subir a la nube //git push
+//para clomar nos paramos en la carpeta donde lo queremos clonar
+//git clone url
+//git ls -la para asgurarme donde estamos
+
+//Pasos
+//git init
+//git add .
